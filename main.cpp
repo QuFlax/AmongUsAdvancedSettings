a567fb97ee0392f3505ba40e1b9c6a9f568588a7
QuFlax <45743113+QuFlax@users.noreply.github.com>
2021-01-21 17:24:12 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,23 +11,25 @@ int main() {
     char* d = new char[] {(char)0x83, (char)0x78, (char)0x74, (char)0x01, (char)0x75};
     char* s = new char[1];
     ifstream inf("GameAssembly.dll");
-    inf.seekg(start);
-    inf.read(s, 1);
-    inf.close();
-    if (s[0] == d[0]) cout << "Patch disabled "; else cout << "Patch enabled ";
-    ofstream outf("GameAssembly.dll");
-    char c;
-    cout << "(e/d):" << endl;
-    cin >> c;
-    if (c == 'e') {
-        outf.seekp(start);
-        outf.write(e, len);
-        outf.close();
-        cout << "Patch enabled" << endl;
-    } else {
-        outf.seekp(start);
-        outf.write(d, len);
-        outf.close();
-        cout << "Patch disabled" << endl;
+    if (inf.is_open()) {
+        inf.seekg(start);
+        inf.read(s, 1);
+        inf.close();
+        if (s[0] == d[0]) cout << "Patch disabled "; else cout << "Patch enabled ";
+        ofstream outf("GameAssembly.dll");
+        char c;
+        cout << "(e/d):" << endl;
+        cin >> c;
+        if (c == 'e') {
+            outf.seekp(start);
+            outf.write(e, len);
+            outf.close();
+            cout << "Patch enabled" << endl;
+        } else {
+            outf.seekp(start);
+            outf.write(d, len);
+            outf.close();
+            cout << "Patch disabled" << endl;
+        }
     }
 }
